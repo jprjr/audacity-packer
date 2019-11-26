5e02608bd344d653a3e94b4a545698f56cebfaf0
John Regan <john@jrjrtech.com>
2019-11-26 12:42:39 -0500

    audparse: switch to iterative parser

--- audparse.c
+++ audparse.c
@@ -7,101 +7,117 @@
 #include "slurp.h"
 #include "yxml.h"
 
-
-long audparse(const char *filename,const char *foldername,filelist *flist) {
-    yxml_t *x;
-    uint8_t *buffer;
-    uint8_t *xml;
-    uint8_t *xmlp;
-    size_t filesize = 0;
-    unsigned int ret = 0;
-    unsigned long rate = 0;
-    char databuf[100];
-    int datapos = 0;
-    yxml_ret_t y = YXML_OK;
-
-    x = NULL;
-    buffer = NULL;
-    xml = NULL;
-
-    x = (yxml_t *)malloc(sizeof(yxml_t));
-    if(x == NULL) {
-        ret = -1;
-        goto error;
+aud_parser *aud_new(void) {
+    aud_parser *parser = (aud_parser *)malloc(sizeof(aud_parser));
+    if(parser == NULL) {
+        return NULL;
     }
-
-
-    xml = slurp(filename,&filesize);
-    if(xml == NULL) {
-        ret = -1;
-        goto error;
+    parser->x = (yxml_t *)malloc(sizeof(yxml_t));
+    if(parser->x == NULL) {
+        aud_free(parser);
+        return NULL;
     }
-    xmlp = xml;
-
-    buffer = (uint8_t *)malloc(8192);
-    if(buffer == NULL) {
-        ret = -1;
-        goto error;
+    parser->buffer = (uint8_t *)malloc(sizeof(uint8_t) * 8192);
+    if(parser->buffer == NULL) {
+        aud_free(parser);
+        return NULL;
     }
-    yxml_init(x,buffer,8192);
+    parser->xml = NULL;
+    parser->xmlsize = 0;
+    yxml_init(parser->x,parser->buffer,8192);
 
-    while((unsigned long)(xmlp - xml) < filesize) {
-        y = yxml_parse(x, *xmlp++);
-        if(y < 0) {
-            ret = -1;
-            goto error;
-        }
-        if(y == YXML_ELEMSTART) {
-            if(strcmp(x->elem,"pcmaliasblockfile") == 0) {
-                ret = -2;
-                goto error;
-            }
-        }
-        else if(y == YXML_ATTRSTART) {
-            datapos = 0;
-            databuf[datapos] = 0;
-        } else if(y == YXML_ATTREND) {
-            databuf[datapos] = 0;
-            if(strcmp(x->elem,"wavetrack") == 0) {
-              if(strcmp(x->attr,"rate") == 0) {
-                  rate = strtoul(databuf,NULL,10);
-              }
-            }
-            else if(strcmp(x->elem,"simpleblockfile") == 0) {
-              if(strcmp(x->attr,"filename") == 0) {
-                  if(filelist_append(flist,databuf,rate) == NULL) {
-                      ret = -1;
-                      goto error;
-                  }
-                  ret++;
-              }
-            }
-            else if(strcmp(x->elem,"project") == 0) {
-              if(strcmp(x->attr,"projname") == 0) {
-                  if(filelist_root(flist,databuf) == NULL) {
-                      ret = -1;
-                      goto error;
-                  }
-              }
-            }
-        }
-        else if(y == YXML_ATTRVAL) {
-            databuf[datapos++] = x->data[0];
-        }
-
-    }
-
-    goto cleanup;
-
-error:
-    switch(ret) {
-        case -1: fprintf(stderr,"error: out of memory\n"); break;
-        case -2: fprintf(stderr,"warning: pcmaliasblockfile found\n"); break;
-    }
-
-cleanup:
-    if(x != NULL) free(x);
-    if(buffer != NULL) free(buffer);
-    if(xml != NULL) free(xml);
-    return ret;
+    return parser;
 }
+
+void aud_unload(aud_parser *parser) {
+    if(parser->xml != NULL) free(parser->xml);
+    parser->xml = NULL;
+    return;
+}
+
+void aud_free(aud_parser *parser) {
+    if(parser != NULL) {
+        aud_unload(parser);
+        if(parser->x != NULL) {
+            free(parser->x);
+        }
+        if(parser->buffer != NULL) {
+            free(parser->buffer);
+        }
+        free(parser);
+        parser = NULL;
+    }
+    return;
+}
+
+int aud_load(aud_parser *parser, const char *filename) {
+    aud_unload(parser);
+
+    parser->xml = slurp(filename, &(parser->xmlsize));
+    if(parser->xml == NULL) {
+        parser->xmlsize = 0;
+        return -1;
+    }
+    parser->xmlpos = 0;
+    parser->datapos = 0;
+    parser->databuf[0] = 0;
+    return 0;
+}
+
+/* -1: unrecorerable error
+ * -2: warning, found a pcmalias
+ *  1: EOF
+ *  0: normal
+ */
+int aud_parse(aud_parser *parser, filelist *flist) {
+    yxml_ret_t y = YXML_OK;
+    if(parser->xml == NULL) return -1;
+
+    if(parser->xmlpos < parser->xmlsize) {
+        y = yxml_parse(parser->x, parser->xml[parser->xmlpos++]);
+    } else if(parser->xmlpos == parser->xmlsize) {
+        y = yxml_eof(parser->x);
+        parser->xmlpos++;
+    } else {
+        return 1;
+    }
+    if(y < 0) {
+        return -1;
+    }
+    if(y == YXML_ELEMSTART) {
+        if(strcmp(parser->x->elem,"pcmaliasblockfile") == 0) {
+            return -1;
+        }
+    }
+    else if(y == YXML_ATTRSTART) {
+        parser->datapos = 0;
+        parser->databuf[parser->datapos] = 0;
+    } else if(y == YXML_ATTREND) {
+        parser->databuf[parser->datapos] = 0;
+        if(strcmp(parser->x->elem,"wavetrack") == 0) {
+          if(strcmp(parser->x->attr,"rate") == 0) {
+              parser->rate = strtoul(parser->databuf,NULL,10);
+          }
+        }
+        else if(strcmp(parser->x->elem,"simpleblockfile") == 0) {
+          if(strcmp(parser->x->attr,"filename") == 0) {
+              if(filelist_append(flist,parser->databuf,parser->rate) == NULL) {
+                  return -1;
+              }
+          }
+        }
+        else if(strcmp(parser->x->elem,"project") == 0) {
+          if(strcmp(parser->x->attr,"projname") == 0) {
+              if(filelist_root(flist,parser->databuf) == NULL) {
+                  return -1;
+              }
+          }
+        }
+    }
+    else if(y == YXML_ATTRVAL) {
+        parser->databuf[parser->datapos++] = parser->x->data[0];
+    }
+    return 0;
+}
+
+
--- audparse.h
+++ audparse.h
@@ -2,12 +2,36 @@
 #define AUDPARSE_H
 
 #include "filelist.h"
+#include "yxml.h"
+#include <stdint.h>
+
+struct aud_parser_s {
+    yxml_t *x;
+    uint8_t *buffer;
+    uint8_t *xml;
+    char databuf[100];
+    size_t datapos;
+    size_t xmlpos;
+    size_t xmlsize;
+    unsigned int rate;
+};
+
+typedef struct aud_parser_s aud_parser;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-long audparse(const char *filename,const char *foldername,filelist *flist);
+aud_parser *aud_new(void);
+void aud_free(aud_parser *parser);
+
+/* return 0 on success, -1 on error */
+int aud_load(aud_parser *parser, const char *filename);
+void aud_unload(aud_parser *parser);
+
+/* return -2 on pcm warning, -1 on error, 0 on success, 1 on complete */
+int aud_parse(aud_parser *parser, filelist *flist);
+
 
 #ifdef __cplusplus
 }
