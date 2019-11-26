f44252027a2f503ca43d04c41af57079735ae55c
John Regan <john@jrjrtech.com>
2019-11-26 12:44:15 -0500

    pack/unpack: use debug logging / better extension changing

--- pack.c
+++ pack.c
@@ -8,18 +8,40 @@ unsigned int pack(filelist *flist, unsigned int i) {
     char *packedfile;
     char *ext;
     unsigned int ret;
+
+    if(flist->files[i].sourcefile == NULL) {
+        DEBUG_LOG("File %u is null, skipping\n",i);
+        return 0;
+    }
+
     packedfile = strdup(flist->files[i].sourcefile);
-    if(packedfile == NULL) return 1;
+    if(packedfile == NULL) {
+        DEBUG_LOG("Out of memory\n");
+        return 1;
+    }
+
     ext = strrchr(packedfile,'.');
     if(ext == NULL) {
+        DEBUG_LOG("Could not find extension in %s\n",packedfile);
         free(packedfile);
         return 1;
     }
-    strcpy(ext,".wv");
+
+    *ext = '\0';
+
+    packedfile = realloc(packedfile,strlen(packedfile) + 4);
+    if(packedfile == NULL) {
+        DEBUG_LOG("Could not realloc packedfile\n");
+        return 1;
+    }
+
+    strcat(packedfile,".wv");
     ret = aupack(flist->files[i].sourcefile,packedfile,flist->files[i].samplerate);
     if(ret == 0) {
+        DEBUG_LOG("Removing %s\n",flist->files[i].sourcefile);
         util_delfile(flist->files[i].sourcefile);
     } else {
+        DEBUG_LOG("Removing %s\n",flist->files[i].packedfile);
         util_delfile(packedfile);
     }
     free(packedfile);
--- unpack.c
+++ unpack.c
@@ -8,20 +8,40 @@ unsigned int unpack(filelist *flist, unsigned int i) {
     char *unpackedfile;
     char *ext;
     unsigned int ret;
-    if(flist->files[i].packedfile == NULL) return 1;
+
+    if(flist->files[i].packedfile == NULL) {
+        DEBUG_LOG("File %u is null, skipping\n",i);
+        return 0;
+    }
 
     unpackedfile = strdup(flist->files[i].packedfile);
-    if(unpackedfile == NULL) return 1;
+    if(unpackedfile == NULL) {
+        DEBUG_LOG("Out of memory\n");
+        return 1;
+    }
+
     ext = strrchr(unpackedfile,'.');
     if(ext == NULL) {
+        DEBUG_LOG("Could not find extension in %s",unpackedfile);
         free(unpackedfile);
         return 1;
     }
-    strcpy(ext,".au");
+
+    *ext = '\0';
+
+    unpackedfile = realloc(unpackedfile,strlen(unpackedfile) + 4);
+    if(unpackedfile == NULL) {
+        DEBUG_LOG("Could not realloc unpackedfile\n");
+        return 1;
+    }
+
+    strcat(unpackedfile,".wv");
     ret = auunpack(flist->files[i].packedfile,unpackedfile);
     if(ret == 0) {
+        DEBUG_LOG("Removing %s\n",flist->files[i].packedfile);
         util_delfile(flist->files[i].packedfile);
     } else {
+        DEBUG_LOG("Removing %s\n",unpackedfile);
         util_delfile(unpackedfile);
     }
     free(unpackedfile);
